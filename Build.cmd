e7627eae15f4f42569dbb037dd832c19284b3474
martincostello <martin@martincostello.com>
2016-07-01 18:50:36 +0100

    Only restore NuGet packages for projects
    Only restore NuGet packages for projects, not any project.json files
    copied to output directories.

--- Build.cmd
+++ Build.cmd
@@ -1,5 +1,5 @@
 @echo off
-dotnet restore --verbosity minimal
+dotnet restore src/API/project.json tests/API.Tests/project.json --verbosity minimal
 dotnet build src/API
 dotnet test tests/API.Tests
 dotnet publish src/API
