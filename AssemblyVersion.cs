8356256ff6e76ce4fdfeae3bb1c5a539f827fbd5
martincostello <martin@martincostello.com>
2016-07-01 18:55:00 +0100

    Add files for shared assembly attributes
    Add C# files for adding shared assembly attributes to projects for #5.

---
+++ AssemblyVersion.cs
@@ -0,0 +1,2 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for full license information.
---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("https://github.com/martincostello/api")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("Martin Costello's API")]
+[assembly: AssemblyTrademark("")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
