e845719cf947e260cfb69008fcb942f565e56fa9
martincostello <martin@martincostello.com>
2017-02-11 15:46:19 +0000

    Migrate to MSBuild
    Migrate from project.json to MSBuild projects using RC4 of the .NET SDK.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+export artifacts=$(dirname "$(readlink -f "$0")")/artifacts
+export configuration=Release
+
 dotnet restore --verbosity minimal || exit 1
-dotnet build src/API || exit 1
-dotnet test tests/API.Tests || exit 1
-dotnet publish src/API || exit 1
+dotnet build --output $artifacts --configuration $configuration || exit 1
+dotnet test tests/API.Tests/API.Tests.csproj --output $artifacts --configuration $configuration || exit 1
+dotnet publish src/API/API.csproj --output $artifacts --configuration $configuration || exit 1
