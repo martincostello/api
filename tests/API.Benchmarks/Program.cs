71388964c6dd789830aa37708472cbddbd81dd0e
martincostello <martin@martincostello.com>
2021-08-22 18:39:00 +0100

    Use top-level program
    Use a top-level program instead of having an explicit Program and Main.

--- tests/API.Benchmarks/Program.cs
+++ tests/API.Benchmarks/Program.cs
@@ -2,36 +2,19 @@
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
 using BenchmarkDotNet.Running;
+using MartinCostello.Api.Benchmarks;
 
-namespace MartinCostello.Api.Benchmarks;
-
-/// <summary>
-/// A console application that runs performance benchmarks for the API. This class cannot be inherited.
-/// </summary>
-internal static class Program
+if (args?.Length == 1 && string.Equals(args[0], "--test", StringComparison.OrdinalIgnoreCase))
 {
-    /// <summary>
-    /// The main entry-point to the application.
-    /// </summary>
-    /// <param name="args">The arguments to the application.</param>
-    /// <returns>
-    /// A <see cref="Task"/> representing the asynchronous invocation of the application.
-    /// </returns>
-    internal static async Task Main(string[] args)
-    {
-        if (args?.Length == 1 && string.Equals(args[0], "--test", StringComparison.OrdinalIgnoreCase))
-        {
-            using var benchmark = new ApiBenchmarks();
-            await benchmark.StartServer();
+    using var benchmark = new ApiBenchmarks();
+    await benchmark.StartServer();
 
-            await benchmark.Hash();
-            await benchmark.Time();
+    await benchmark.Hash();
+    await benchmark.Time();
 
-            await benchmark.StopServer();
-        }
-        else
-        {
-            BenchmarkRunner.Run<ApiBenchmarks>(args: args);
-        }
-    }
+    await benchmark.StopServer();
+}
+else
+{
+    BenchmarkRunner.Run<ApiBenchmarks>(args: args);
 }
