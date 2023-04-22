269c2ab85b83c077aa48bb75020255135f568761
martincostello <martin@martincostello.com>
2023-04-22 09:03:52 +0100

    Remove redundant suppressions
    Remove now-redundant suppressions for false-positive code analysis warnings.

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
-#pragma warning disable CA1852
-
 using MartinCostello.Api;
 
 var builder = WebApplication.CreateBuilder(args);
