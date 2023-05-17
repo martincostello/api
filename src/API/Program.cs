e9a61d1582506036a28c41c6ac00d67b454ad950
costellobot <102549341+costellobot@users.noreply.github.com>
2023-05-17 07:41:45 +0100

    Update .NET SDK to 7.0.302 (#1123)
    * Update .NET SDK
    
    Update .NET SDK to version 7.0.302.
    
    ---
    updated-dependencies:
    - dependency-name: Microsoft.NET.Sdk
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: costellobot <102549341+costellobot@users.noreply.github.com>
    
    * Suppress CA1852
    
    Suppress false-positive for `CA1852` reintroduced by the .NET 7.0.302 SDK.
    
    * Suppress CA1852
    
    Suppress false-positive for `CA1852` reintroduced by the .NET 7.0.302 SDK.
    
    ---------
    
    Signed-off-by: costellobot <102549341+costellobot@users.noreply.github.com>
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
+#pragma warning disable CA1852
+
 using MartinCostello.Api;
 
 var builder = WebApplication.CreateBuilder(args);
