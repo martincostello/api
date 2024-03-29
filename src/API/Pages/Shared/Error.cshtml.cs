1b7b6444c9ccbcbac09b96f8f916daa28cd0b859
martincostello <martin@martincostello.com>
2024-03-29 09:41:42 +0000

    Solution improvements
    - Enable (almost) all built-in style analyzers.
    - Fix new style warnings.
    - Use global package references.
    - Add reference trimmer analyzer.

--- src/API/Pages/Shared/Error.cshtml.cs
+++ src/API/Pages/Shared/Error.cshtml.cs
@@ -1,26 +1,24 @@
 ﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
-#pragma warning disable CS1591
 #pragma warning disable SA1600
 #pragma warning disable SA1649
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MartinCostello.Api.Pages
+namespace MartinCostello.Api.Pages;
+
+public class ErrorModel : PageModel
 {
-    public class ErrorModel : PageModel
+    public int ErrorStatusCode { get; set; } = StatusCodes.Status500InternalServerError;
+
+    public void OnGet(int? id)
     {
-        public int ErrorStatusCode { get; set; } = StatusCodes.Status500InternalServerError;
-
-        public void OnGet(int? id)
+        if (id is { } status && status >= 400 && status < 599)
         {
-            if (id is { } status && (status >= 400 && status < 599))
-            {
-                ErrorStatusCode = status;
-            }
-
-            Response.StatusCode = ErrorStatusCode;
+            ErrorStatusCode = status;
         }
+
+        Response.StatusCode = ErrorStatusCode;
     }
 }
