ce41a5e810f07ccb67f4e981deaac3c498ab0cae
martincostello <martin@martincostello.com>
2024-06-04 17:32:39 +0100

    Customise OpenAPI document
    Add custom code to transform the OpenAPI document with examples, descriptions, etc.

---
+++ src/API/OpenApi/AddResponseDescriptionTransformer.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace MartinCostello.Api.OpenApi;
+
+/// <summary>
+/// Represents a document processor that adds descriptions to the responses of API endpoints. This class cannot be inherited.
+/// </summary>
+internal sealed class AddResponseDescriptionTransformer : IOpenApiOperationTransformer
+{
+    /// <inheritdoc/>
+    public Task TransformAsync(
+        OpenApiOperation operation,
+        OpenApiOperationTransformerContext context,
+        CancellationToken cancellationToken)
+    {
+        var attributes = context.Description.ActionDescriptor.EndpointMetadata.OfType<OpenApiResponseAttribute>().ToArray();
+
+        foreach (var attribute in attributes)
+        {
+            if (operation.Responses.TryGetValue(attribute.HttpStatusCode.ToString(CultureInfo.InvariantCulture), out var response))
+            {
+                response.Description = attribute.Description;
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
