84d2cdf7b611116b4eef8694d724742d72122268
martincostello <martin@martincostello.com>
2024-06-11 12:27:27 +0100

    Lint OpenAPI schema
    Lint the new OpenAPI schema too.

--- tests/API.Tests/Integration/OpenApiTests.cs
+++ tests/API.Tests/Integration/OpenApiTests.cs
@@ -15,6 +15,7 @@ namespace MartinCostello.Api.Integration;
 public class OpenApiTests(TestServerFixture fixture, ITestOutputHelper outputHelper) : IntegrationTest(fixture, outputHelper)
 {
     [Theory]
+    [InlineData("/swagger/api/openapi.json")]
     [InlineData("/swagger/api/swagger.json")]
     public async Task Static_And_Dynamic_Schema_Match(string subpath)
     {
