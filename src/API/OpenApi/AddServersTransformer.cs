b2955216a67d149279079ce24e13a9457680748b
martincostello <martin@martincostello.com>
2024-06-11 12:42:30 +0100

    Add servers
    Manually add the `servers` property to the response until it is added automatically.

---
+++ src/API/OpenApi/AddServersTransformer.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+
+namespace MartinCostello.Api.OpenApi;
+
+/// <summary>
+/// A class representing a document processor that server information. This class cannot be inherited.
+/// </summary>
+internal sealed class AddServersTransformer : IOpenApiDocumentTransformer
+{
+    /// <inheritdoc/>
+    public Task TransformAsync(
+        OpenApiDocument document,
+        OpenApiDocumentTransformerContext context,
+        CancellationToken cancellationToken)
+    {
+        document.Servers = [new() { Url = "https://api.martincostello.com" }];
+        return Task.CompletedTask;
+    }
+}
