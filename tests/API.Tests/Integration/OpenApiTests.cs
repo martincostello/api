98c011d0535dfa542a3c9bb9928ec7d7b71e0892
Martin Costello <martin@martincostello.com>
2024-07-28 13:12:58 +0100

    Generate OpenAPI document (#1873)
    Generate the OpenAPI document at build time, rather than remembering to put a copy of it on disk.

--- tests/API.Tests/Integration/OpenApiTests.cs
+++ tests/API.Tests/Integration/OpenApiTests.cs
@@ -1,13 +1,9 @@
 ﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
-using FluentAssertions.Json;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Readers;
 using Microsoft.OpenApi.Validations;
-using Newtonsoft.Json.Linq;
 
 namespace MartinCostello.Api.Integration;
 
@@ -30,51 +26,8 @@ public class OpenApiTests(TestServerFixture fixture, ITestOutputHelper outputHel
         await VerifyJson(actual, settings);
     }
 
-    [Theory]
-    [InlineData("/openapi/api.json")]
-    public async Task Static_And_Dynamic_Schema_Match(string subpath)
-    {
-        // Arrange
-        var requestUri = new Uri(subpath, UriKind.Relative);
-
-        var environment = Fixture.Services.GetRequiredService<IWebHostEnvironment>();
-
-        var schema = environment.WebRootFileProvider.GetFileInfo(subpath);
-        schema.Exists.ShouldBeTrue();
-
-        using var stream = schema.CreateReadStream();
-        using var reader = new StreamReader(stream);
-        string expectedJson = await reader.ReadToEndAsync();
-
-        using var client = Fixture.CreateClient();
-
-        // Act
-        string actualJson = await client.GetStringAsync(requestUri);
-
-        // Use System.Text.Json equivalent when available.
-        // See https://github.com/fluentassertions/fluentassertions/issues/2205.
-        var expected = JToken.Parse(expectedJson);
-        var actual = JToken.Parse(actualJson);
-
-        // Servers is removed because the URL won't neccessarily match.
-        expected["servers"]?.Parent?.Remove();
-        actual["servers"]?.Parent?.Remove();
-
-        // Assert
-        string customMessage =
-            $"""
-            The static OpenAPI schema differs from the dynamically generated one.
-            If the changes are intentional, please update the static schema file at
-            '{subpath}' with the latest content returned by an HTTP GET
-            to the {requestUri} endpoint.
-            """;
-
-        actual.Should().BeEquivalentTo(expected, customMessage.Trim());
-    }
-
-    [Theory]
-    [InlineData("/openapi/api.json")]
-    public async Task Schema_Has_No_Validation_Warnings(string requestUrl)
+    [Fact]
+    public async Task Schema_Has_No_Validation_Warnings()
     {
         // Arrange
         var ruleSet = ValidationRuleSet.GetDefaultRuleSet();
@@ -85,7 +38,7 @@ public class OpenApiTests(TestServerFixture fixture, ITestOutputHelper outputHel
         using var client = Fixture.CreateClient();
 
         // Act
-        using var schema = await client.GetStreamAsync(requestUrl);
+        using var schema = await client.GetStreamAsync("/openapi/api.json");
 
         // Assert
         var reader = new OpenApiStreamReader();
