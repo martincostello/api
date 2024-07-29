db13352775f91c73bb8d8637ad41894067a93ba5
martincostello <martin@martincostello.com>
2024-07-29 11:10:38 +0100

    Use MartinCostello.OpenApi.Extensions
    Use MartinCostello.OpenApi.Extensions NuGet package to extend OpenAPI document.

--- src/API/OpenApi/ProblemDetailsExampleProvider.cs
+++ src/API/OpenApi/ProblemDetailsExampleProvider.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
+using MartinCostello.OpenApi;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MartinCostello.Api.OpenApi;
