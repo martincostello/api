f1af6649aed46718f6ecac61ffb84988aba14d98
martincostello <martin@martincostello.com>
2024-10-27 15:13:02 +0000

    Run dotnet format
    Run dotnet format and commit changes.

--- tests/API.Tests/Integration/TestServerCollection.cs
+++ tests/API.Tests/Integration/TestServerCollection.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Martin Costello, 2016. All rights reserved.
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for full license information.
 
 namespace MartinCostello.Api.Integration;
