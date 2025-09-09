16cc824f7e8fa2fbc03ae489f861ab242d581ff3
Martin Costello <martin@martincostello.com>
2025-09-09 20:57:11 +0100

    Fix test
    Fix test (again) by updating the expected media type for YAML.

--- tests/API.Tests/EndToEnd/ResourceTests.cs
+++ tests/API.Tests/EndToEnd/ResourceTests.cs
@@ -24,7 +24,7 @@ public class ResourceTests(ApiFixture fixture) : EndToEndTest(fixture)
     [InlineData("/humans.txt", MediaTypeNames.Text.Plain)]
     [InlineData("/keybase.txt", MediaTypeNames.Text.Plain)]
     [InlineData("/openapi/api.json", MediaTypeNames.Application.Json)]
-    [InlineData("/openapi/api.yaml", MediaTypeNames.Application.Yaml)]
+    [InlineData("/openapi/api.yaml", "text/plain+yaml")]
     [InlineData("/robots.txt", MediaTypeNames.Text.Plain)]
     [InlineData("/robots933456.txt", MediaTypeNames.Text.Plain)]
     [InlineData("/sitemap.xml", MediaTypeNames.Text.Xml)]
