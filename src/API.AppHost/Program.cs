9709b81a113cb18d0fef7cc150ed8f57f958fab7
martincostello <martin@martincostello.com>
2025-12-11 13:16:57 +0000

    Add Aspire
    Add an Aspire app host for local development.

---
+++ src/API.AppHost/Program.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for full license information.
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+const string Project = "API";
+
+builder.AddProject<Projects.API>(Project)
+       .WithHttpHealthCheck("/version");
+
+var app = builder.Build();
+
+app.Run();
